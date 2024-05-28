0c9670bb92c30a886918d1abbc3a742891a645ac
agusCod3 <agus.romero.salazar@gmail.com>
2024-05-27 23:09:28 -0400

    pRIMER COMMIT AL PROYECTO , ADAPTER LISTO

---
+++ app/src/main/java/com/example/reconocedorrazasapp/doglist/DogAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.reconocedorrazasapp.doglist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.reconocedorrazasapp.Dog
+import com.example.reconocedorrazasapp.databinding.DogListItemBinding
+
+class DogAdapter: ListAdapter<Dog, DogAdapter.DogViewwHolder>(DiffCallback) {
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Dog>() {
+        override fun areItemsTheSame(oldItem: Dog, newItem: Dog): Boolean {
+            // Compara los ID o cualquier propiedad única que identifique al ítem
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Dog, newItem: Dog): Boolean {
+            // Compara los contenidos de los ítems
+            return oldItem == newItem
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewwHolder {
+
+        val binding = DogListItemBinding.inflate(LayoutInflater.from(parent.context))
+        return DogViewwHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: DogViewwHolder, position: Int) {
+
+        val dog = getItem(position)
+        holder.bindDog(dog)
+    }
+
+    inner class DogViewwHolder(val binding: DogListItemBinding): RecyclerView.ViewHolder(binding.root){
+        fun bindDog(dog: Dog){
+            binding.txtDogName.text = dog.name
+        }
+    }
+}
