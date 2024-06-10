7afecd4148f670c3d9997b80bb786d2560094dcf
agusCod3 <agus.romero.salazar@gmail.com>
2024-06-09 23:01:15 -0400

    Validaciones para el formulario de la creación de una página

--- app/src/main/java/com/example/reconocedorrazasapp/presentation/auth/SignUpFragment.kt
+++ app/src/main/java/com/example/reconocedorrazasapp/presentation/auth/SignUpFragment.kt
@@ -1,6 +1,7 @@
 package com.example.reconocedorrazasapp.presentation.auth
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,7 +24,50 @@ class SignUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSignUpBinding.inflate(inflater)
+        setUpSignUpButton()
         return binding.root
+    }
+
+    private fun setUpSignUpButton() {
+        binding.signUpButton.setOnClickListener {
+            validateFields()
+        }
+    }
+
+    private fun validateFields(){
+
+        binding.emailInput.error = ""
+        binding.passwordInput.error = ""
+        binding.confirmPasswordInput.error = ""
+
+        val email = binding.emailEdit.text.toString()
+
+        if(!isValidEmail(email)){
+            binding.emailInput.error = getString(R.string.mail_is_not_valid)
+            return
+        }
+
+        val password = binding.passwordEdit.text.toString()
+        if( password.isEmpty() ){
+            binding.passwordInput.error = getString(R.string.password_is_not_empty)
+            return
+        }
+
+        val passwordConfirmation = binding.confirmPasswordEdit.text.toString()
+        if(passwordConfirmation.isEmpty() ){
+           binding.confirmPasswordInput.error = getString(R.string.confirm_password_is_not_empty)
+           return
+        }
+
+        if (password != passwordConfirmation){
+            binding.passwordInput.error = getString(R.string.passwords_do_not_match)
+            return
+        }
+    }
+
+    private fun isValidEmail(email: String?): Boolean{
+        return !email.isNullOrEmpty() &&
+                Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
 }
